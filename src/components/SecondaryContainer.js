27b570c15ba0249ac87cbe8fa87e453005aaadf5
ajayypatil <yadava701@gmail.com>
2023-10-05 13:13:27 +0530

    Secondary Container for movie-lists

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
+  if (movies.nowPlayingMovies === null) return;
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    <div className="bg-black">
+      <div className="relative -mt-52 z-10 pl-12">
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Popular Movies" movies={movies.popularMovies} />
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+      </div>
+    </div>
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
